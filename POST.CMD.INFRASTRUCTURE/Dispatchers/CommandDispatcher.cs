8c451dd254adfa8af51ba4a09e3e9ef1bda51c48
Trần Minh Quân <minhquantb00@gmail.com>
2024-06-24 23:06:58 +0700

    init

--- POST.CMD.INFRASTRUCTURE/Dispatchers/CommandDispatcher.cs
+++ POST.CMD.INFRASTRUCTURE/Dispatchers/CommandDispatcher.cs
@@ -13,7 +13,10 @@ namespace POST.CMD.INFRASTRUCTURE.Dispatchers
         private readonly Dictionary<Type, Func<BaseCommand, Task>> _handlers = new();
         public void RegisterHandler<T>(Func<T, Task> handler) where T : BaseCommand
         {
-            throw new NotImplementedException();
+            if (_handlers.ContainsKey(typeof(T)))
+            {
+                throw new IndexOutOfRangeException("You cannot register the same command handle twice");
+            }
         }
 
         public Task SendAsync(BaseCommand command)
