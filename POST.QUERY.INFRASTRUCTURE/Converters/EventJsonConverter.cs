46b4612419b56454768e45be2ee8454ea26851a6
Trần Minh Quân <minhquantb00@gmail.com>
2024-06-27 22:53:04 +0700

    init

---
+++ POST.QUERY.INFRASTRUCTURE/Converters/EventJsonConverter.cs
@@ -0,0 +1,28 @@
+﻿using CQRS.CORE.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace POST.QUERY.INFRASTRUCTURE.Converters
+{
+    public class EventJsonConverter : JsonConverter<BaseEvent>
+    {
+        public override bool CanConvert(Type typeToConvert)
+        {
+            return typeToConvert.IsAssignableFrom(typeof(BaseEvent));
+        }
+        public override BaseEvent? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Write(Utf8JsonWriter writer, BaseEvent value, JsonSerializerOptions options)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
