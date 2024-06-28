20b66cea7db088f3cae91d1d57f1e7fed8fe3604
Trần Minh Quân <minhquantb00@gmail.com>
2024-06-28 10:39:45 +0700

    update evenJsonConverter

--- POST.QUERY.INFRASTRUCTURE/Converters/EventJsonConverter.cs
+++ POST.QUERY.INFRASTRUCTURE/Converters/EventJsonConverter.cs
@@ -1,4 +1,5 @@
 ﻿using CQRS.CORE.Events;
+using POST.COMMONS.Events;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,6 +7,7 @@ using System.Text;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
+using ThirdParty.Json.LitJson;
 
 namespace POST.QUERY.INFRASTRUCTURE.Converters
 {
@@ -17,7 +19,30 @@ namespace POST.QUERY.INFRASTRUCTURE.Converters
         }
         public override BaseEvent? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
         {
-            throw new NotImplementedException();
+            if(!JsonDocument.TryParseValue(ref reader, out var doc))
+            {
+                throw new System.Text.Json.JsonException($"Failed to parse {nameof(JsonDocument)}");
+            }
+
+            if(!doc.RootElement.TryGetProperty("Type", out var type))
+            {
+                throw new System.Text.Json.JsonException("Could not detect the Type discriminator property!");
+            }
+            var typeDiscriminator = type.GetString();
+            var json = doc.RootElement.GetRawText();
+
+            return typeDiscriminator switch
+            {
+                nameof(PostCreatedEvent) => JsonSerializer.Deserialize<PostCreatedEvent>(json, options),
+                nameof(MessageUpdatedEvent) => JsonSerializer.Deserialize<MessageUpdatedEvent>(json, options),
+                nameof(PostLikeEvent) => JsonSerializer.Deserialize<PostLikeEvent>(json, options),
+                nameof(CommentAddedEvent) => JsonSerializer.Deserialize<CommentAddedEvent>(json, options),
+                nameof(CommentUpdatedEvent) => JsonSerializer.Deserialize<CommentUpdatedEvent>(json, options),
+                nameof(CommentRemovedEvent) => JsonSerializer.Deserialize<CommentRemovedEvent>(json, options),
+                nameof(PostRemovedEvent) => JsonSerializer.Deserialize<PostRemovedEvent>(json, options),
+                _ => throw new System.Text.Json.JsonException($"{typeDiscriminator} is not supported yet!")
+
+            };
         }
 
         public override void Write(Utf8JsonWriter writer, BaseEvent value, JsonSerializerOptions options)
