6951a7d3cf081e8ac42fe38b543bf32443d6edf3
Trần Minh Quân <minhquantb00@gmail.com>
2024-09-09 21:37:04 +0700

    update

--- POST.CMD.INFRASTRUCTURE/Producers/EventProducer.cs
+++ POST.CMD.INFRASTRUCTURE/Producers/EventProducer.cs
@@ -24,18 +24,29 @@ namespace POST.CMD.INFRASTRUCTURE.Producers
                 .SetKeySerializer(Serializers.Utf8)
                 .SetValueSerializer(Serializers.Utf8)
                 .Build();
+
             var eventMessage = new Message<string, string>
             {
                 Key = Guid.NewGuid().ToString(),
                 Value = JsonSerializer.Serialize(@event, @event.GetType())
             };
 
-            var deliveryResult = await producer.ProduceAsync(topic, eventMessage);
-
-            if(deliveryResult.Status == PersistenceStatus.NotPersisted)
+            try
             {
-                throw new Exception($"Could not produce {@event.GetType().Name} message to topic - {topic} due to the following reason: {deliveryResult.Message}.");
+                var deliveryResult = await producer.ProduceAsync(topic, eventMessage);
+
+                if (deliveryResult.Status == PersistenceStatus.NotPersisted)
+                {
+                    throw new Exception($"Could not produce {@event.GetType().Name} message to topic - {topic} due to the following reason: {deliveryResult.Message}.");
+                }
+
+                Console.WriteLine($"Message produced to topic: {topic} | Partition: {deliveryResult.Partition} | Offset: {deliveryResult.Offset}");
+            }
+            catch (ProduceException<string, string> e)
+            {
+                Console.WriteLine($"Delivery failed: {e.Error.Reason}");
             }
         }
+
     }
 }
